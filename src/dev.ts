a2671aa80c568989f032929d187fd1e7609b87ee
JustSteve2k <srodriguez2k@gmail.com>
2022-08-26 23:58:33 -0500

    init

---
+++ src/dev.ts
@@ -0,0 +1,14 @@
+// Used for adding money buttons.  Mostly for testing.
+export function AddMoney(amount: number) {
+  let currency = document.getElementById("currency");
+  if (currency != null) {
+    let value = parseInt(currency.innerText);
+    value += amount;
+
+    currency.innerText = value.toString();
+  }
+}
+
+export function NotReady() {
+  alert("This function is not implemented yet.");
+}
