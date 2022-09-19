7f9784f3a93b208d6b155966c1fb7595b5e3aea6
JustSteve2k <srodriguez2k@gmail.com>
2022-09-18 22:33:33 -0500

    init

---
+++ src/values.ts
@@ -0,0 +1,27 @@
+// Gets values of all the infantry.
+// Returns an object with all the values.
+export function GetValues() {
+  const values = { infantrymen: 0, machineguns: 0, turrets: 0, cannons: 0, gunships: 0, battleships: 0, sateliteguns: 0, spaceships: 0 };
+
+  values.infantrymen = parseInt(document.getElementById("infantrymen")!.innerText);
+  values.machineguns = parseInt(document.getElementById("machineguns")!.innerText);
+  values.turrets = parseInt(document.getElementById("turrets")!.innerText);
+  values.cannons = parseInt(document.getElementById("cannons")!.innerText);
+  values.gunships = parseInt(document.getElementById("gunships")!.innerText);
+  values.battleships = parseInt(document.getElementById("battleships")!.innerText);
+  values.sateliteguns = parseInt(document.getElementById("sateliteguns")!.innerText);
+  values.spaceships = parseInt(document.getElementById("spaceships")!.innerText);
+
+  return values;
+}
+
+// Gets game stats.
+export function getStats() {
+  const values = { level: 0, zombies: 0, currency: 0 };
+
+  values.level = parseInt(document.getElementById("level")!.innerText);
+  values.zombies = parseInt(document.getElementById("zombies")!.innerText);
+  values.zombies = parseInt(document.getElementById("currency")!.innerText);
+
+  return values;
+}
