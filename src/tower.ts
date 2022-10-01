8f90c11a6eb2dc7e440090d0579c66dc1eacc4b8
JustSteve2k <srodriguez2k@gmail.com>
2022-09-30 21:36:31 -0500

    initial tower class

---
+++ src/tower.ts
@@ -0,0 +1,25 @@
+export default class Tower {
+  public health: number;
+  public id: number;
+  private defence: number;
+
+  // Future things to add: innate attack.  Maintenance cost?
+
+  constructor(health: number, id: number) {
+    this.health = health;
+    this.id = id;
+  }
+
+  lowerHealth(amount: number): void {
+    this.health -= amount;
+  }
+
+  raiseHealth(amount: number): void {
+    this.health -= amount;
+  }
+
+  outputInfo() {
+    console.log(`${this.id} - Tower ID`);
+    console.log(`${this.health} - Tower Health`);
+  }
+}
